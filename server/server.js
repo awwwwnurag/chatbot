9e78d6116eb4f634c5cacdf09de153a2b655417f
NityaDubeyyy <nityadubeyyy@gmail.com>
2026-04-06 20:21:34 +0530

    fix deployment white screen and server crash

--- server/server.js
+++ server/server.js
@@ -42,12 +42,13 @@ app.use('/api/credit', creditRouter)
 
 // SERVE CLIENT SIDE FILES
 app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
+// Triggering restart to load new .env variables
 server.listen(PORT, () => {
     console.log(`
     ==================================================
